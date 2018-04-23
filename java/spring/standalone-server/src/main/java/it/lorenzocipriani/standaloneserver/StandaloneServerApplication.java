dee22c7b03a94efa3bdb2bd19dd81a07c1b0c8df
lorenzo <lorenzo@192.168.0.108>
2018-04-24 00:46:24 +0100

    Created Spring Boot standalone server boilerplate

---
+++ java/spring/standalone-server/src/main/java/it/lorenzocipriani/standaloneserver/StandaloneServerApplication.java
@@ -0,0 +1,12 @@
+package it.lorenzocipriani.standaloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandaloneServerApplication.class, args);
+	}
+}
---
+++ java/spring/standalone-server/src/test/java/it/lorenzocipriani/standaloneserver/StandaloneServerApplicationTests.java
@@ -0,0 +1,16 @@
+package it.lorenzocipriani.standaloneserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StandaloneServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
